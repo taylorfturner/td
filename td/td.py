a8b81557905d2d1828a9c1b449caf4b7385e2020
Taylor Turner <taylorfturner@gmail.com>
2021-06-21 20:42:45 -0400

    inital pass on class methods for primary UX

--- td/td.py
+++ td/td.py
@@ -8,4 +8,25 @@ class TD:
         :return: [description]
         :rtype: [type]
         """
-        return pass
+        pass
+
+
+    def get(self):
+        """
+        Get all the account(s) data from the TD Ameritrade API.
+        """
+        pass
+
+
+    def put(self):
+        """
+        append data to a local copy of all prior queries
+        """
+        pass
+
+    def show(self):
+        """
+        This should return a dash plotly dashboard with all pertinent data 
+        and graphs.
+        """
+        pass
