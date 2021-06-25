0b02d1dbf2f7db259bc12176d6b1f1d72efd5ecb
Taylor Turner <taylorfturner@gmail.com>
2021-06-24 20:09:58 -0400

    update single file UX

--- td/td.py
+++ td/td.py
@@ -1,4 +1,5 @@
 import requests
+from td.authentication import Authenticator
 
 
 class TD:
@@ -9,14 +10,29 @@ class TD:
         :return: [description]
         :rtype: [type]
         """
-        pass
+        self.endpoint = "https://api.tdameritrade.com/v1/accounts"
 
 
-    def get(self):
+    def get(self, params={"fields": "positions"}):
         """
         Get all the account(s) data from the TD Ameritrade API.
         """
-        pass
+
+        auth = Authenticator()
+
+        header = {
+            "Authorization": f"Bearer {auth.token.access_token}"
+        }
+
+        content = requests.get(
+            url = self.endpoint,
+            params = params,
+            headers = header
+        )
+
+        data = content.json()
+
+        return data
 
 
     def put(self):
