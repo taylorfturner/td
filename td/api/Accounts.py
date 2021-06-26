1fbb04dbdde8e9ec31f5f42286f9119c04159567
Taylor Turner <taylorfturner@gmail.com>
2021-06-26 18:49:39 -0400

    processing data logic

--- td/api/Accounts.py
+++ td/api/Accounts.py
@@ -1,7 +1,8 @@
 from datetime import date
 import requests
+import pandas as pd
 import json
-from td.auth import Authenticator
+from td.auth.Authenticator import Authenticator
 
 
 class Accounts:
@@ -81,13 +82,38 @@ class Accounts:
         self.put()
         return True
 
-    def _process_positions_dict(self, position_dict):
-
-        instrument = position_dict["instrument"]
-
-        position_dict.pop("instrument", None)
-
-        position_dict["symbol"] = instrument["symbol"]
-        position_dict["asset_type"] = instrument["assetType"]
-
+    def process_data(self):
         
+        account_meta_df = pd.DataFrame()
+        position_df = pd.DataFrame()
+        
+        for iter_data in self.data:
+            iter_data = iter_data["securitiesAccount"]
+
+            # account meta data
+            type = iter_data["type"]
+            account_id = iter_data["accountId"]
+            roundTrips = iter_data["roundTrips"]
+            is_day_trader = iter_data["isDayTrader"]
+            is_closing_only_restricted = iter_data["isClosingOnlyRestricted"]
+            initial_balances = iter_data["initialBalances"]
+            current_balances = iter_data["currentBalances"]
+            projected_balance = iter_data["projectedBalances"]
+
+            # account's position data
+            positions = iter_data["positions"]
+            for single_position in positions:
+                instrument = single_position["instrument"]
+                single_position.pop("instrument", None)
+                single_position["symbol"] = instrument["symbol"]
+                single_position["asset_type"] = instrument["assetType"]
+                single_position["account_id"] = account_id
+
+                single_position_list = list(single_position)
+
+                if len(position_df) < 1:
+                    position_df = position_df.from_dict(single_position, orient='columns')
+                elif len(position_df) >= 1:
+                    position_df = position_df.append(single_position_list)
+
+            return account_meta_df, position_df
